446e2ec4a19c29c4c3ad3df8e3ff9cd4b17126ea
Tea <icepie.dev@gmail.com>
2021-05-13 02:28:58 +0800

    del netdump

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,5 @@
 
 // NAPT example released to public domain
-
-#if LWIP_FEATURES && !LWIP_IPV6
-
-#define HAVE_NETDUMP 0
-
 #ifndef STASSID
 #define STASSID "mynetwork"
 #define STAPSK  "mynetworkpassword"
@@ -18,31 +13,10 @@
 #define NAPT 1000
 #define NAPT_PORT 10
 
-#if HAVE_NETDUMP
-
-#include <NetDump.h>
-
-void dump(int netif_idx, const char* data, size_t len, int out, int success) {
-  (void)success;
-  Serial.print(out ? F("out ") : F(" in "));
-  Serial.printf("%d ", netif_idx);
-
-  // optional filter example: if (netDump_is_ARP(data))
-  {
-    netDump(Serial, data, len);
-    //netDumpHex(Serial, data, len);
-  }
-}
-#endif
-
 void setup() {
   Serial.begin(115200);
   Serial.printf("\n\nNAPT Range extender\n");
   Serial.printf("Heap on start: %d\n", ESP.getFreeHeap());
-
-#if HAVE_NETDUMP
-  phy_capture = dump;
-#endif
 
   // first, connect to STA so we can get a proper local DNS server
   WiFi.mode(WIFI_STA);
@@ -82,15 +56,6 @@ void setup() {
     Serial.printf("NAPT initialization failed\n");
   }
 }
-
-#else
-
-void setup() {
-  Serial.begin(115200);
-  Serial.printf("\n\nNAPT not supported in this configuration\n");
-}
-
-#endif
 
 void loop() {
 }
