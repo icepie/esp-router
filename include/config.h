f16e9828d881087d43acfd7a47b2e2d77f774c24
Tea <icepie.dev@gmail.com>
2021-05-13 02:53:50 +0800

    add config.h

---
+++ include/config.h
@@ -0,0 +1,12 @@
+#ifndef STASSID
+#define STASSID "mynetwork"
+#define STAPSK  "mynetworkpassword"
+#endif
+
+#ifndef APSSID
+#define APSSID "icepie-iot-"
+#define APPSK  ""
+#endif
+
+#define NAPT 1000
+#define NAPT_PORT 10
--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,69 @@
 
 // NAPT example released to public domain
-#ifndef STASSID
-#define STASSID "mynetwork"
-#define STAPSK  "mynetworkpassword"
-#endif
 
 #include <ESP8266WiFi.h>
 #include <lwip/napt.h>
 #include <lwip/dns.h>
 #include <LwipDhcpServer.h>
 
-#define NAPT 1000
-#define NAPT_PORT 10
+// config
+#include "config.h"
+
+void setSTA(const char *ssid, const char *passphrase) {
+
+  WiFi.begin(ssid, passphrase);
+
+  if (WiFi.status() == WL_CONNECTED) {
+    // give DNS servers to AP side
+    dhcpSoftAP.dhcps_set_dns(0, WiFi.dnsIP(0));
+    dhcpSoftAP.dhcps_set_dns(1, WiFi.dnsIP(1));
+  }
+
+}
 
 void setup() {
   Serial.begin(115200);
   Serial.printf("\n\nNAPT Range extender\n");
-  Serial.printf("Heap on start: %d\n", ESP.getFreeHeap());
+  //Serial.printf("Heap on start: %d\n", ESP.getFreeHeap());
 
   // first, connect to STA so we can get a proper local DNS server
   WiFi.mode(WIFI_STA);
   WiFi.begin(STASSID, STAPSK);
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print('.');
-    delay(500);
-  }
+  // while (WiFi.status() != WL_CONNECTED) {
+  //   Serial.print('.');
+  //   delay(500);
+  // }
+
   Serial.printf("\nSTA: %s (dns: %s / %s)\n",
                 WiFi.localIP().toString().c_str(),
                 WiFi.dnsIP(0).toString().c_str(),
                 WiFi.dnsIP(1).toString().c_str());
 
-  // give DNS servers to AP side
-  dhcpSoftAP.dhcps_set_dns(0, WiFi.dnsIP(0));
-  dhcpSoftAP.dhcps_set_dns(1, WiFi.dnsIP(1));
+  if (WiFi.status() == WL_CONNECTED) {
+    // give DNS servers to AP side
+    dhcpSoftAP.dhcps_set_dns(0, WiFi.dnsIP(0));
+    dhcpSoftAP.dhcps_set_dns(1, WiFi.dnsIP(1));
+  }
 
   WiFi.softAPConfig(  // enable AP, with android-compatible google domain
-    IPAddress(172, 217, 28, 254),
-    IPAddress(172, 217, 28, 254),
+    IPAddress(192, 168, 9, 1),
+    IPAddress(192, 168, 9, 1),
     IPAddress(255, 255, 255, 0));
-  WiFi.softAP(STASSID "extender", STAPSK);
+
+  WiFi.softAP(APSSID "extender", APPSK);
   Serial.printf("AP: %s\n", WiFi.softAPIP().toString().c_str());
 
-  Serial.printf("Heap before: %d\n", ESP.getFreeHeap());
+  //Serial.printf("Heap before: %d\n", ESP.getFreeHeap());
   err_t ret = ip_napt_init(NAPT, NAPT_PORT);
-  Serial.printf("ip_napt_init(%d,%d): ret=%d (OK=%d)\n", NAPT, NAPT_PORT, (int)ret, (int)ERR_OK);
+  //Serial.printf("ip_napt_init(%d,%d): ret=%d (OK=%d)\n", NAPT, NAPT_PORT, (int)ret, (int)ERR_OK);
   if (ret == ERR_OK) {
     ret = ip_napt_enable_no(SOFTAP_IF, 1);
-    Serial.printf("ip_napt_enable_no(SOFTAP_IF): ret=%d (OK=%d)\n", (int)ret, (int)ERR_OK);
+    //Serial.printf("ip_napt_enable_no(SOFTAP_IF): ret=%d (OK=%d)\n", (int)ret, (int)ERR_OK);
     if (ret == ERR_OK) {
-      Serial.printf("WiFi Network '%s' with same password is now NATed behind '%s'\n", STASSID "extender", STASSID);
+      Serial.printf("WiFi router is running!");
     }
   }
-  Serial.printf("Heap after napt init: %d\n", ESP.getFreeHeap());
+  //Serial.printf("Heap after napt init: %d\n", ESP.getFreeHeap());
   if (ret != ERR_OK) {
     Serial.printf("NAPT initialization failed\n");
   }
